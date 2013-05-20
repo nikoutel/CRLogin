c30b0b12d41c78eed05a20b735f25765ae1f3bcd
Nikos Koutelidis <nikoutel@gmail.com>
2013-05-20 13:25:26 +0200

    added _getTables(), _getColumns for MySQL, SQLite

--- MySQLDatabase.php
+++ MySQLDatabase.php
@@ -1,9 +1,36 @@
 <?php
+
 class MySQLDatabase extends Database {
 
     function __construct($utils, $dsn, $user, $passwd, array $options) {
         parent::__construct($utils, $dsn, $user, $passwd, $options);
     }
 
+    protected function _getTables() {
+        $sqlQuery = "SHOW TABLES";
+        
+        $array = $this->runQuery($sqlQuery);
+        foreach ($array as $value) {
+            $tables[] = $value[key($value)];
+        }
+        return $tables;
+    }
+
+    protected function _getColumns($tableName, $showPrimaryKey = FALSE) {
+        if (in_array($tableName, $this->_getTables())) {
+            $sqlQuery = "SHOW COLUMNS FROM " . $tableName;
+            
+            $array = $this->runQuery($sqlQuery);
+            foreach ($array as $value) {
+                if (($showPrimaryKey) || ($value['Key']) != 'PRI') {
+                    reset($value);
+                    $columns[] = $value[key($value)];
+                }
+            } return $columns;
+        } else
+            return FALSE;
+    }
+
 }
+
 ?>
--- SQLiteDatabase.php
+++ SQLiteDatabase.php
@@ -1,9 +1,40 @@
 <?php
+
 // !!! the folder the database resides in must have write permissions, as well as the actual database file.
 class SQLiteDatabase extends Database {
 
     function __construct($utils, $dsn, $user, $passwd, $options) {
         parent::__construct($utils, $dsn, $user, $passwd, $options);
+    }
+
+    protected function _getTables() {
+        $sqlQuery = "SELECT name FROM 
+                    (SELECT * FROM sqlite_master UNION ALL
+                    SELECT * FROM sqlite_temp_master)
+                    WHERE type='table'
+                    ORDER BY name";
+
+        $array = $this->runQuery($sqlQuery);
+        foreach ($array as $value) {
+            $tables[] = $value[key($value)];
+        }
+        return $tables;
+    }
+
+    protected function _getColumns($tableName, $showPrimaryKey = FALSE) {
+        if (in_array($tableName, $this->_getTables())) {
+            $sqlQuery = "PRAGMA table_info('" . $tableName . "');";
+
+            $array = $this->runQuery($sqlQuery);
+            foreach ($array as $value) {
+                if (($showPrimaryKey) || ($value['pk']) != '1') {
+                    $columns[] = $value['name'];
+                }
+            }
+            return $columns;
+        }
+        else
+            return FALSE;
     }
 
 }
