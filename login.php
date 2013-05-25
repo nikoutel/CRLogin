33e402341350a576ae4bd45a303a1952b9daba26
Nikos Koutelidis <nikoutel@gmail.com>
2013-05-25 15:23:36 +0200

    new file:   Challenge.php new file:   Response.php new file:   User.php new file:   login.php new file:   request.php new file:   scripts/jquery-1.7.1.min.js new file:   scripts/scripts.js

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Login</title>
+        <script type="text/javascript" src="scripts/scripts.js"></script>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>       
+        <script type="text/javascript">
+            if (typeof jQuery === 'undefined') {
+                document.write(unescape("%3Cscript src='/scripts/jquery-1.7.1.min.js' type='text/javascript'%3E%3C/script%3E"));
+            }
+        </script>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
