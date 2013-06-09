c1612075662e1eace8a28964b043da7a1c046f97
Nikos Koutelidis <nikoutel@gmail.com>
2013-06-09 12:06:01 +0200

    DataAccess, no main

--- CRLogin/core/DataAccess/MySQLDatabase.php
+++ CRLogin/core/DataAccess/MySQLDatabase.php
@@ -1,13 +1,43 @@
 <?php
 
+/**
+ *
+ * MySQLDatabase:
+ *
+ *
+ * @package CRLogin
+ * @subpackage core/Data Access
+ * @author Nikos Koutelidis nikoutel@gmail.com
+ * @copyright 2013 Nikos Koutelidis
+ * @license http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link https://github.com/nikoutel/CRLogin
+ *
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
 namespace CRLogin\core\DataAccess;
 
 class MySQLDatabase extends PDODatabase {
 
+    /**
+     *
+     * @param array $dbParameters
+     * @param Utils $utils
+     */
     function __construct(array $dbParameters, $utils) {
         parent::__construct($dbParameters, $utils);
     }
 
+    /**
+     * A MySQL specific method
+     * Returns the database tables
+     *
+     * @return array
+     */
     protected function _getTables() {
         $sqlQuery = "SHOW TABLES";
 
@@ -18,6 +48,17 @@ class MySQLDatabase extends PDODatabase {
         return $tables;
     }
 
+    /**
+     * A MySQL specific method
+     * Returns the columns of table $tableName
+     *
+     * If $withPrimaryKey is true the primary key is also included.
+     * Default is false
+     *
+     * @param string $tableName
+     * @param boolean $withPrimaryKey
+     * @return mixed
+     */
     protected function _getColumns($tableName, $withPrimaryKey = FALSE) {
         if (in_array($tableName, $this->_getTables())) {
             $sqlQuery = "SHOW COLUMNS FROM " . $tableName;
