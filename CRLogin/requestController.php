10b364b252700427c990e48cd9bcd5622bc54a4b
Nikos Koutelidis <nikoutel@gmail.com>
2013-06-10 14:09:17 +0200

    root; main; scripts

--- CRLogin/requestController.php
+++ CRLogin/requestController.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ *
+ * requestController.php
+ * Controls  and handles all the POST requests.
+ * 
+ * 
+ * @package CRLogin
+ * @author Nikos Koutelidis nikoutel@gmail.com
+ * @copyright 2013 Nikos Koutelidis 
+ * @license http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link https://github.com/nikoutel/CRLogin 
+ * 
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ * 
+ */
+
 namespace CRLogin;
 
 use CRLogin\core\DIC;
--- CRLoginAutoloader.php
+++ CRLoginAutoloader.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ *
+ * CRLoginAutoloader.php
+ * Autoloader for CRLogin
+ * 
+ * 
+ * @package CRLogin
+ * @author Nikos Koutelidis nikoutel@gmail.com
+ * @copyright 2013 Nikos Koutelidis 
+ * @license http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link https://github.com/nikoutel/CRLogin 
+ * 
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ * 
+ */
+
 function CRLoginAutoloader($className) {
     $className = ltrim($className, '\\');
     $fileName = '';
