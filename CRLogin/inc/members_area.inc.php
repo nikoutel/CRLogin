3df634353a9b6d41203300632ad1bde56806dc25
Nikos Koutelidis <nikoutel@gmail.com>
2013-06-10 13:53:37 +0200

    inc

--- CRLogin/inc/members_area.inc.php
+++ CRLogin/inc/members_area.inc.php
@@ -1,5 +1,25 @@
 <?php
 
+/**
+ *
+ * members_area_inc.inc
+ * Include file to be included on top of all members area pages
+ * 
+ * 
+ * @package CRLogin
+ * @subpackage inc
+ * @author Nikos Koutelidis nikoutel@gmail.com
+ * @copyright 2013 Nikos Koutelidis 
+ * @license http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
+ * @link https://github.com/nikoutel/CRLogin 
+ * 
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ * 
+ */
+
 use CRLogin\core\DIC;
 use CRLogin\core\Crypt;
 
