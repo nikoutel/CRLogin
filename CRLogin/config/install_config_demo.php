dd78d842d5762083a84c8c5abc69184065799198
Nikos Koutelidis <nikoutel@gmail.com>
2014-06-24 12:39:14 +0200

    added installUniqueId in intstall_config_demo.php

--- CRLogin/config/install_config_demo.php
+++ CRLogin/config/install_config_demo.php
@@ -34,7 +34,8 @@ $db_config = array(
         )
     ),
     'general' => array(
-        'language' => 'en'
+        'language' => 'en',
+        'installUniqueId' => '3u2Sok4VePf5e6SQ5kHXkQ' // CHANGE THIS
     )
 );
 
@@ -50,7 +51,8 @@ $db_config = array(
         )
     ),
     'general' => array(
-        'language' => 'en'
+        'language' => 'en',
+        'installUniqueId' => '3u2Sok4VePf5e6SQ5kHXkQ' // CHANGE THIS
     )
 );
  
