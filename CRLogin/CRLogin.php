e4d9765c9df0f2a23828ba7a35873d749b54a384
Nikos Koutelidis <nikoutel@gmail.com>
2019-04-08 15:54:08 +0200

    checks if CRL_BASE_DIR and CRL_APP_DIR are corectly set

--- CRLogin/CRLogin.php
+++ CRLogin/CRLogin.php
@@ -22,13 +22,24 @@ if (!function_exists('getToken')) {
     }
 }
 
-//@todo test CRL_BASE_DIR with call; if not ajax
+if (!isAjax()) {
+    if (!defined('CRL_BASE_DIR')){
+        exit("Error: CRL_BASE_DIR not set!");
+    }
+    if (!defined('CRL_APP_DIR')){
+        exit("Error: CRL_APP_DIR not set!");
+    }
+    if (!file_exists(CRL_BASE_DIR . DIRECTORY_SEPARATOR . CRL_APP_DIR . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php')) {
+        exit("Error: CRL_BASE_DIR and/or CRL_APP_DIR not correctly set!");
+    }
+} else {
+    if (!defined('CRL_BASE_DIR')) define('CRL_BASE_DIR', $_SERVER['DOCUMENT_ROOT'] . '/' . dirname(dirname($_SERVER['SCRIPT_NAME'])));
+    if (!defined('CRL_APP_DIR')) define('CRL_APP_DIR', basename(dirname($_SERVER['SCRIPT_NAME'])));
+}
 
 $baseURL = dirname("//{$_SERVER['HTTP_HOST']}{$_SERVER['SCRIPT_NAME']}");
-if (!defined('CRL_BASE_URL'))define('CRL_BASE_URL', $baseURL);
+if (!defined('CRL_BASE_URL')) define('CRL_BASE_URL', $baseURL);
 
-if (!defined('CRL_BASE_DIR')) define('CRL_BASE_DIR', $_SERVER['DOCUMENT_ROOT'] . '/' . dirname(dirname($_SERVER['SCRIPT_NAME'])));
-if (!defined('CRL_APP_DIR')) define('CRL_APP_DIR', basename(dirname($_SERVER['SCRIPT_NAME'])));
 require_once CRL_BASE_DIR . '/' . CRL_APP_DIR . '/CRLoginAutoloader.php';
 
 if (!isset($dic)) {
@@ -52,7 +63,7 @@ if (!isAjax()) {
     if ($_SESSION ['members']) {
         $_SESSION['redirectURL'] = '//' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; //@todo not safe
         if (!isset($_SESSION['logged']) || ($_SESSION['logged'] === FALSE)) {
-        header('Location:index.php?s=login'); //@todo decouple from demo
+            header('Location:index.php?s=login'); //@todo decouple from demo
             die();
         }
     } else {
