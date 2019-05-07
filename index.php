f233593c3896e24b4e7b0cc2ca1eb6720993e03e
Nikos Koutelidis <nikoutel@gmail.com>
2019-05-07 08:28:35 +0200

    added public demo page

--- index.php
+++ index.php
@@ -4,20 +4,20 @@
  *
  * index.php
  * A basic entry point.
- * 
- * 
+ *
+ *
  * @package CRLogin
  * @subpackage demo
  * @author Nikos Koutelidis nikoutel@gmail.com
  * @copyright 2013-2019 Nikos Koutelidis
  * @license http://www.mozilla.org/MPL/2.0/ Mozilla Public License Version 2.0
- * @link https://github.com/nikoutel/CRLogin 
- * 
- * 
+ * @link https://github.com/nikoutel/CRLogin
+ *
+ *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
- * 
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
  */
 
 $isMembersArea = false;
@@ -31,13 +31,16 @@ if (isset($_GET['s'])) {
             //  js handles logout
             break;
         case 'members':
-            include  'demo/members_area.php';
+            include 'demo/members_area.php';
             break;
         case 'changepassword':
             include 'demo/changepassword.php';
             break;
         case 'register':
             include 'demo/register.php';
+            break;
+        case 'public':
+            include 'demo/public.php';
             break;
         default:
             include 'demo/main.php';
