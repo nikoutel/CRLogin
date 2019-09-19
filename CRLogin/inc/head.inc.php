55fec09667f0f062111c5fec69de189093157a28
Nikos Koutelidis <nikoutel@gmail.com>
2019-09-19 15:46:00 +0200

    Remove type attribute from script tag; remove jquery CDN call

--- CRLogin/inc/head.inc.php
+++ CRLogin/inc/head.inc.php
@@ -22,12 +22,12 @@
 
 $url = htmlspecialchars(CRL_APP_URL_PATH . CRL_APP_DIR);
 ?>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>       
-<script type="text/javascript">
-    if (typeof jQuery === 'undefined') {
-        document.write(unescape("%3Cscript src='<?= $url ?>/scripts/jquery-1.7.1.min.js' type='text/javascript'%3E%3C/script%3E"));
-    }
-</script>
-<script type="text/javascript" src="<?= $url ?>/scripts/jBCrypt/bCrypt.js"></script>
-<script type="text/javascript" src="<?= $url ?>/scripts/sha256.js"></script>
-<script type="text/javascript" src="<?= $url ?>/scripts/scripts.js"></script>
+    <script>
+        if (typeof jQuery === 'undefined') {
+            document.write(unescape("%3Cscript src='<?= $url ?>/scripts/jquery-1.7.1.min.js' type='text/javascript'%3E%3C/script%3E"));
+        }
+    </script>
+    <script src="<?= $url ?>/scripts/msg.js.php"></script>
+    <script src="<?= $url ?>/scripts/jBCrypt/bCrypt.js"></script>
+    <script src="<?= $url ?>/scripts/sha256.js"></script>
+    <script src="<?= $url ?>/scripts/scripts.js"></script>
